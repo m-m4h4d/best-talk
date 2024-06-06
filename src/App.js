db11758010ece0a0ccdd052b36338b0b8b71dcc1
Muhammad Mahad <mahadbinashfaq@gmail.com>
2024-06-06 20:20:28 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import image from './image.png';
 import logo from './logo.png';
+import { East } from '@mui/icons-material';
 import './App.css';
 import Button from '@mui/material/Button';
 
@@ -8,42 +9,44 @@ function App() {
   return (
     <body>
       <div className="App">
-        <nav>
-          <div className="logo">
-            <img src={logo} alt='hm'/>
-          </div>
-          <ul>
-            <li><Button onClick={() => {
-              alert('clicked');
-            }} variant="text" style={{ color: 'black' }} sx={{ borderRadius: 28 }}>Courses</Button></li>
-            <li><Button onClick={() => {
-              alert('clicked');
-            }} variant="text" style={{ color: 'black' }} sx={{ borderRadius: 28 }}>About</Button></li>
-            <li><Button onClick={() => {
-              alert('clicked');
-            }} variant="text" style={{ color: 'black' }} sx={{ borderRadius: 28 }}>Contact</Button></li>
-            <li><Button onClick={() => {
-              alert('clicked');
-            }} variant="text" style={{ color: 'black' }} sx={{ borderRadius: 28 }}>Login</Button></li>
-            <li><Button onClick={() => {
-              alert('clicked');
-            }} variant="contained" sx={{ borderRadius: 28 }}>Sign Up</Button></li>
-          </ul>
-        </nav>
-        <header>
-          <div className='left'>
-            <h1><span style={{color: "purple"}}>Empower</span> your<br>
-            </br>learning journey<br>
-            </br>with the <span>best tutors.</span></h1>
-            <p></p>
-            <Button onClick={() => {
-              alert('clicked');
-            }} variant="contained" sx={{ borderRadius: 28 }}>Go with your tutor</Button>
-          </div>
-          <div className='right'>
-            <img src={image} alt='huh'/>
-          </div>
-        </header>
+        <div className='landing'>
+          <nav>
+            <img src={logo} alt='logo' />
+            <ul>
+              <div className='buttons'>
+                <Button onClick={() => {
+                  alert('clicked');
+                }} variant="text" style={{ color: 'black', textTransform: 'none' }} sx={{ borderRadius: 28 }}>Courses</Button>
+                <Button onClick={() => {
+                  alert('clicked');
+                }} variant="text" style={{ color: 'black', textTransform: 'none' }} sx={{ borderRadius: 28 }}>About</Button>
+                <Button onClick={() => {
+                  alert('clicked');
+                }} variant="text" style={{ color: 'black', textTransform: 'none' }} sx={{ borderRadius: 28 }}>Contact</Button>
+                <Button onClick={() => {
+                  alert('clicked');
+                }} variant="text" style={{ color: 'black', textTransform: 'none' }} sx={{ borderRadius: 28 }}>Login</Button>
+                <Button onClick={() => {
+                  alert('clicked');
+                }} variant="contained" style={{ background: '#2196D4', textTransform: 'none' }} sx={{ borderRadius: 28 }}>Sign Up</Button>
+              </div>
+            </ul>
+          </nav>
+          <header>
+            <div className='left'>
+              <h1><span className='empower'>Empower</span> your<br></br>learning journey<br></br>with the <span className='tutor'>best tutors.</span></h1>
+              <p>Our online tutors offer personalized, one-on-one learning to help<br></br>you improve your grades, build your confidence, and achieve your<br></br>academic goals.</p>
+              <Button onClick={() => {
+                alert('clicked');
+              }} variant="contained" sx={{ borderRadius: 28 }} style={{ background: '#2196D4', textTransform: 'none' }} endIcon={<East />}>
+                Go with your tutor
+              </Button>
+            </div>
+            <div className='right'>
+              <img src={image} alt='landing' />
+            </div>
+          </header>
+        </div>
       </div>
     </body>
   );
