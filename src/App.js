27b16331debe52547944b15328d9e27f967fc575
Muhammad Mahad <mahadbinashfaq@gmail.com>
2024-06-06 16:57:04 +0500

    Created Navigation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
 function App() {
   return (
     <div className="App">
+      <nav>
+        <Stack spacing={2} direction="row">
+          <Button variant="text" href='#'>Courses</Button>
+          <Button variant="text" href='#'>About</Button>
+          <Button variant="text" href='#'>Contact</Button>
+          <Button variant="text" href='#'>Login</Button>
+          <Button variant="contained">Sign Up</Button>
+        </Stack>
+      </nav>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
