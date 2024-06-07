413936280c3e85966157fb8c127c1481ae575c39
Muhammad Mahad <mahadbinashfaq@gmail.com>
2024-06-07 16:40:56 +0500

    Comments

--- src/App.js
+++ src/App.js
@@ -8,10 +8,13 @@ import Button from '@mui/material/Button';
 function App() {
   return (
     <div className='body'>
+
+      {/* Home Page */}
       <div className="App">
 
-        
+        {/* Landing Page */}
         <div className='landing'>
+          {/* Group 2 */}
           <nav>
             <img src={logo} alt='logo' />
             <ul>
@@ -34,7 +37,6 @@ function App() {
               </div>
             </ul>
           </nav>
-
           <header>
             <div className='left'>
               <h1><span className='empower'>Empower</span> your<br></br>learning journey<br></br>with the <span className='tutor'>best tutors.</span></h1>
@@ -51,8 +53,15 @@ function App() {
           </header>
         </div>
 
+        {/* Page 3 */}
+        <div className='choose'>
+          <h1>Why <span className='empower'>choose us</span>?</h1>
+        </div>
 
+        {/* Page 5 */}
         <div className='trial'>
+          <h1>Get <span className='empower'>Free Trial</span> Lesson</h1>
+          <p>A free lesson is guaranteed to every student, independent of the number<br></br>of lessons he or she would like to receive.</p>
           <Button onClick={() => {
             alert('clicked');
           }} variant="contained" sx={{ borderRadius: 28 }} style={{ background: '#2196D4', textTransform: 'none' }} endIcon={<East />}>
@@ -60,7 +69,7 @@ function App() {
           </Button>
         </div>
 
-
+        {/* Page 4 */}
         <div className='boost'>
           <div className='grid'>
             <h1>Want to <span className='empower'>boost</span> your <span className='tutor'>tutoring</span> career?</h1>
@@ -73,7 +82,7 @@ function App() {
           </div>
         </div>
 
-
+        {/* Page 7 */}
         <footer></footer>
       </div>
     </div>
