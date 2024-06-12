187188d632b7571a25d894754d7b2e44efbfc939
Muhammad Mahad <mahadbinashfaq@gmail.com>
2024-06-12 14:01:14 +0500

    Form Handling

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -3,6 +3,17 @@ import { Box, Button, Container, TextField, Typography, Link as MuiLink } from '
 import { Link, useNavigate } from 'react-router-dom';
 
 function Signup() {
+    const [values, setValues] = React.useState({
+        firstName: '',
+        lastName: '',
+        emailAddress: '',
+        password: '',
+    });
+
+    const handleChange = (prop) => (event) => {
+        setValues({ ...values, [prop]: event.target.value });
+    };
+
     const navigate = useNavigate();
 
     const handleSubmit = (event) => {
@@ -22,12 +33,60 @@ function Signup() {
                         <MuiLink component={Link} to='/login' style={{ textDecoration: 'none', color: 'black' }}>Log in</MuiLink>
                     </Typography>
                     <form onSubmit={handleSubmit}>
-                        <TextField required fullWidth margin='normal' id='firstName' label='First Name' size='small' style={{ background: '#E8EDF5', color: '#4A789C' }} />
-                        <TextField required fullWidth margin='normal' id='lastName' label='Last Name' size='small' style={{ background: '#E8EDF5', color: '#4A789C' }} />
-                        <TextField required fullWidth margin='normal' id='emailAddress' label='Email Address' type='email' size='small' style={{ background: '#E8EDF5', color: '#4A789C' }} />
-                        <TextField required fullWidth margin='normal' id='password' label='Password' type='password' size='small' style={{ background: '#E8EDF5', color: '#4A789C' }} />
-                        <TextField required fullWidth margin='normal' id='confirmPassword' label='Confirm Password' type='password' size='small' style={{ background: '#E8EDF5', color: '#4A789C' }} />
-                        <Button type='submit' variant='contained' style={{ borderRadius: '12px', margin: '1rem', background: '#2196D4' }}>Continue</Button>
+                        <TextField
+                            required
+                            fullWidth
+                            margin='normal'
+                            id='firstName'
+                            label='First Name'
+                            size='small'
+                            style={{ background: '#E8EDF5', color: '#4A789C' }}
+                        />
+                        <TextField
+                            required
+                            fullWidth
+                            margin='normal'
+                            id='lastName'
+                            label='Last Name'
+                            size='small'
+                            style={{ background: '#E8EDF5', color: '#4A789C' }}
+                        />
+                        <TextField
+                            required
+                            fullWidth
+                            margin='normal'
+                            id='emailAddress'
+                            label='Email Address'
+                            type='email'
+                            size='small'
+                            style={{ background: '#E8EDF5', color: '#4A789C' }}
+                        />
+                        <TextField
+                            required
+                            fullWidth
+                            margin='normal'
+                            id='password'
+                            label='Password'
+                            type='password'
+                            size='small'
+                            style={{ background: '#E8EDF5', color: '#4A789C' }}
+                        />
+                        <TextField
+                            required
+                            fullWidth
+                            margin='normal'
+                            id='confirmPassword'
+                            label='Confirm Password'
+                            type='password'
+                            size='small'
+                            style={{ background: '#E8EDF5', color: '#4A789C' }}
+                        />
+                        <Button
+                            type='submit'
+                            variant='contained'
+                            style={{ borderRadius: '12px', margin: '1rem', background: '#2196D4' }}>
+                            Continue
+                        </Button>
                     </form>
                     <Typography variant="body2" align='center' textAlign='center' color='#4A789C' style={{ margin: '0.25rem' }}>
                         By clicking on continue, you agree to our terms of service and privacy policy
