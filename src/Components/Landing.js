9ae477f7b51376cd6f8d62bcde9fcedb895dc522
Muhammad Mahad <mahadbinashfaq@gmail.com>
2024-06-12 16:19:26 +0500

    Button Functioning

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import * as React from 'react';
 import { Button, Container, Grid, Typography } from '@mui/material';
 import { East } from '@mui/icons-material';
 import Navbar from './Navbar';
 import image from '../Images/image.png';
 import '../App.css';
+import { useNavigate } from 'react-router-dom';
 
 const Landing = () => {
+    const navigate = useNavigate();
+
+    const handleNavigation = (category) => {
+        navigate('/signup', { state: { category } });
+    };
+
     return (
         <div className='landing'>
             <Navbar />
@@ -13,21 +20,28 @@ const Landing = () => {
                 <Grid container spacing={2} alignItems="center">
                     <Grid item xs={12} md={6}>
                         <Typography variant="h2">
-                            <span className='pink'>Empower</span> your<br />learning journey<br />with the <span className='blue'>best tutors.</span>
+                            <span className='pink'>Empower</span> your<br />
+                            learning journey<br />with the 
+                            <span className='blue'>best tutors.</span>
                         </Typography>
                         <Typography variant="body1" paragraph>
-                            Our online tutors offer personalized, one-on-one learning to help you improve your grades, build your confidence, and achieve your academic goals.
+                            Our online tutors offer personalized, one-on-one learning to help 
+                            you improve your grades, build your confidence, and achieve your 
+                            academic goals.
                         </Typography>
                         <Button
-                            onClick={() => alert('clicked')}
+                            onClick={() => handleNavigation('Learner')}
                             variant="contained"
-                            sx={{ borderRadius: 28, background: '#2196D4', textTransform: 'none' }}
+                            sx={{ borderRadius: 28,
+                                background: '#2196D4',
+                                textTransform: 'none' }}
                             endIcon={<East />}>
                             Go with your tutor
                         </Button>
                     </Grid>
                     <Grid item xs={12} md={6}>
-                        <img src={image} alt='landing' style={{ width: '100%', borderRadius: '30px' }} />
+                        <img src={image} alt='landing'
+                        style={{ width: '100%', borderRadius: '30px' }} />
                     </Grid>
                 </Grid>
             </Container>
--- src/Components/Trial.js
+++ src/Components/Trial.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import * as React from 'react';
 import "../App.css";
 import { Button, Container, Grid, Typography } from '@mui/material';
 import { East } from '@mui/icons-material';
+import { useNavigate } from 'react-router-dom';
 
 const Trial = () => {
+    const navigate = useNavigate();
+
+    const handleNavigation = (category) => {
+        navigate('/signup', { state: { category } });
+    };
+
     return (
         <div className='trial'>
             <Container>
@@ -11,16 +18,20 @@ const Trial = () => {
                     Get <span className='pink'>Free Trial</span> Lesson
                 </Typography>
                 <Typography variant="body1" align="center" paragraph>
-                    A free lesson is guaranteed to every student, independent of the number<br />of lessons he or she would like to receive.
+                    A free lesson is guaranteed to every student,
+                    independent of the number<br />of lessons he or she would like to receive.
                 </Typography>
                 <Grid container justifyContent="center">
                     <Grid item>
                         <Button
-                            onClick={() => alert('clicked')}
+                            onClick={() => handleNavigation('Learner')}
                             variant="contained"
-                            sx={{ borderRadius: 28, background: '#2196D4', textTransform: 'none' }}
-                            endIcon={<East />}
-                        >
+                            sx={{
+                                borderRadius: 28,
+                                background: '#2196D4',
+                                textTransform: 'none'
+                            }}
+                            endIcon={<East />}>
                             Sign Up Now
                         </Button>
                     </Grid>
